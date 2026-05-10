e51b0aa8dbc9a64b93e62a80c1cdbfc41e08a48b
Mahalaxmi K <mahalaxmi.kumbari@gmail.com>
2026-05-11 02:22:01 +0530

    chore: scaffold Angular 17 standalone project

---
+++ mylife-app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  ownerEmail: 'YOUR_EMAIL@example.com'
+};
---
+++ mylife-app/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  ownerEmail: 'YOUR_EMAIL@example.com'
+};
